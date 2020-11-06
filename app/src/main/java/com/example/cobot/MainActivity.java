fe2b84b162ebdcc8b530764df96be41270190eab
Xieee <justinjayxie@gmail.com>
2020-11-06 14:34:47 -0800

    Nav should be working

--- app/src/main/java/com/example/cobot/MainActivity.java
+++ app/src/main/java/com/example/cobot/MainActivity.java
@@ -17,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.nav_home:
+                        break;
+                    case R.id.nav_chat:
+                        Intent iChat = new Intent(MainActivity.this, ChatActivity.class);
+                        startActivity(iChat);
+                        break;
+                    case R.id.nav_schedule:
+                        Intent iSched = new Intent(MainActivity.this, ScheduleActivity.class);
+                        startActivity(iSched);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
